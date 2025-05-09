86809a9e893a6d71b301566b8c1a939d6e7c0e30
Eric Alfred <alfrederic371@gmail.com>
2025-05-10 00:17:02 +0100

    fix: updated allowed host

--- kampos/settings.py
+++ kampos/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
